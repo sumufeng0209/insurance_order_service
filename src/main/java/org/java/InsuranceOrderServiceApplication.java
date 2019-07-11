3ccfaed3164df1eef210d0a385e1d564c8c58fb1
sumufeng0209 <1796382617@qq.com>
2019-07-12 00:04:19 +0800

    order_service_commit1

---
+++ src/main/java/org/java/InsuranceOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package org.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceOrderServiceApplication.class, args);
+    }
+
+}
